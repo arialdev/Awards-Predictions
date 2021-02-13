08b3ebe0a5fca1a23f8c8856f3194ce3683e251b
Álvaro Rivas <alvarora12@gmail.com>
2021-02-13 01:40:41 +0100

    server development started

---
+++ server/server.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/awardsModels'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/AwardsPredictions'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/awardsRoutes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
